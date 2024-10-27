4aa80ce5224346152fa9423250e474eaafff842f
kalpana udara <kalpanaudara058@gmail.com>
2024-10-27 20:29:18 +0530

    Set basic config for sanctum, create role_as column into users table

---
+++ tests/Feature/UserSignInTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+final class UserSignInTest extends TestCase
+{
+    public function test_admin_user_can_sign_into_the_system()
+    {
+        // A - Arrange
+
+
+        // A - Action
+
+
+        // A - Assertion
+
+    }
+}
