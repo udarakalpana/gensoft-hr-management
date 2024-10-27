98f514a74525439d309a88a1c0a444976567b020
kalpana udara <kalpanaudara058@gmail.com>
2024-10-27 20:34:11 +0530

    Add role_as column definition into the user model and factory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'role_as' => 1, // 1 = admin user
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
