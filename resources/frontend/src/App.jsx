a981cc1df3197014aebe9352acf450504c66a7d5
kalpana udara <kalpanaudara058@gmail.com>
2024-11-05 08:50:37 +0530

    Create front end login params

--- resources/frontend/src/App.jsx
+++ resources/frontend/src/App.jsx
@@ -1,4 +1,10 @@
 import Routers from "./utlities/routers/Routers.jsx";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+axios.defaults.withCredentials = true;
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.post['Accept'] = 'application/json'
 
 const App = () => {
 
